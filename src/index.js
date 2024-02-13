74e90b711aff3c215562476be8a6d6a27c70193c
Lucas Chaves Meyles <lchavesmeyles@gmail.com>
2024-02-13 13:44:11 -0800

    Updated tab switching logic

--- src/index.js
+++ src/index.js
@@ -2,6 +2,38 @@ import homeContent from './home.js';
 import menuContent from './menu.js';
 import aboutContent from './about.js';
 
+
+const content = document.querySelector('#content');
 const material = homeContent();
-document.body.appendChild(material);
-console.log(material);
+content.appendChild(material);
+console.log(content);
+
+// tab switching logic
+
+const homeButton = document.querySelector('#home');
+homeButton.addEventListener('click', () => {
+    content.replaceChildren();
+    content.appendChild(homeContent());
+});
+
+const menuButton = document.querySelector('#menu');
+menuButton.addEventListener('click', () => {
+    content.replaceChildren();
+    content.appendChild(menuContent());
+});
+
+const aboutButton = document.querySelector('#about');
+aboutButton.addEventListener('click', () => {
+    content.replaceChildren();
+    content.appendChild(aboutContent());
+});
+
+
+
+function switchTabs(button) {
+    // clear the div and then populate with stuff
+    content.replaceChildren();
+    const mat = button();
+    content.appendChild(mat);
+
+}
